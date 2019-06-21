637191d31d6b4c720ccb865b8ae3e61dd43d3c3e
Taewoong Moon <moondingie5@gmail.com>
2019-06-22 03:44:55 +0900

    #2.9 라우터 분리

--- app.js
+++ app.js
@@ -3,29 +3,20 @@ import morgan from 'morgan'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
-import { userRouter } from './router';
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
+
 const app = express()
 
-const handleHome = (req, res) => {res.send('hello world')}
-
-const handleProfile = (req, res) => {res.send('profile')}
-
-const betweenHome = (req, res, next) => {
-    console.log('im between')
-    next()
-}
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 app.use(helmet())
 
-app.use(betweenHome)
-
-app.get('/', handleHome)
-
-app.get('/profile', handleProfile)
-
-app.use('/user', userRouter)
+app.use('/', globalRouter);
+app.use('/user', userRouter);
+app.use('/video', videoRouter);
 
 export default app;
