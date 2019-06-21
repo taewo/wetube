679e82faaa75dfc234b5336473f55b4767c24a44
Taewoong Moon <moondingie5@gmail.com>
2019-06-22 04:20:37 +0900

    #2.11 Controller 추가

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import routes from '../routes';
+import { home, search } from '../controllers/videoController';
+import { login, join, logout } from '../controllers/userController';
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, (req, res) => res.send('Home'));
-globalRouter.get(routes.join, (req, res) => res.send('JOIN'));
-globalRouter.get(routes.login, (req, res) => res.send('LOGIN'));
-globalRouter.get(routes.logout, (req, res) => res.send('LOGOUT'));
-globalRouter.get(routes.search, (req, res) => res.send('SEARCH'));
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
 
 
 export default globalRouter;
