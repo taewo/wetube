9997052f1576614d8f4f7acfbd0df27a231208af
Taewoong Moon <moondingie5@gmail.com>
2019-11-17 10:33:16 +0900

    #2.17 템플릿에 변수 담기

--- middlewares.js
+++ middlewares.js
@@ -1,9 +1,7 @@
 import routes from "./routes";
 
 export const localsMiddleware = (req, res, next) => {
-    console.log('localsMiddleware');
-    
     res.locals.siteName = 'Wetube'
     res.locals.routes = routes
-    // next()
+    next()
 }
