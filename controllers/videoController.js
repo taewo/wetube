3d2d5b3e88a93861d03d0ee92e4c04c1b6f43371
Taewoong Moon <moondingie5@gmail.com>
2019-11-20 22:46:13 +0900

    #2.21 더미데이터를 home.pug로 보내서 화면에 보여줌

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,7 @@
+import { videos } from '../db'
+
 export const home = (req, res) => {
-    res.render('home', { pageTitle: 'Home' })
+    res.render('home', { pageTitle: 'Home', videos })
 }
 export const search = (req, res) => {
     const {
