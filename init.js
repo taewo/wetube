406a8d24ef247e15dc142189e6ecdd46b03aa79b
Taewoong Moon <moondingie5@gmail.com>
2019-11-25 16:06:54 +0900

    #3.1 Connecting to MongoDB

--- init.js
+++ init.js
@@ -1,9 +1,10 @@
+import './db'
 import app from './app'
 
 const PORT = 4000
 
 const handleListening = () => {
-    console.log(`Listening on: http://localhost:${PORT}`)
+    console.log(`✅  Listening on: http://localhost:${PORT}`)
 }
 
 app.listen(PORT, handleListening)
