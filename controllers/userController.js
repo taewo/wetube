2d7b28afab7049c0da3310cc31aac8dc95b7aaaf
Taewoong Moon <moondingie5@gmail.com>
2019-11-25 14:56:46 +0900

    #2.25 More Controlers

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,8 +25,10 @@ export const postLogin = (req, res) => {
     res.redirect(routes.home)
 }
 
-export const logout = (req, res) =>
-    res.render('logout', { pageTitle: 'Log Out' })
+export const logout = (req, res) => {
+    // TODO: process out
+    res.redirect(routes.home)
+}
 export const userDetail = (req, res) =>
     res.render('userDetail', { pageTitle: 'User Detail' })
 export const editProfile = (req, res) =>
