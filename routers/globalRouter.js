fc6eb282f32341b4eef60b68ff4e73652eea3597
Taewoong Moon <moondingie5@gmail.com>
2019-11-25 14:11:54 +0900

    #2.24 login get,post컨트롤러 적용 | id값 여부에 따라 주소값 변경되게 수정

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,16 +1,23 @@
 import express from 'express'
 import routes from '../routes'
 import { home, search } from '../controllers/videoController'
-import { login, getJoin, postJoin, logout } from '../controllers/userController'
+import {
+    getLogin,
+    postLogin,
+    getJoin,
+    postJoin,
+    logout,
+} from '../controllers/userController'
 
 const globalRouter = express.Router()
 
 globalRouter.get(routes.join, getJoin)
 globalRouter.post(routes.join, postJoin)
+globalRouter.get(routes.login, getLogin)
+globalRouter.post(routes.login, postLogin)
 
 globalRouter.get(routes.home, home)
 globalRouter.get(routes.search, search)
-globalRouter.get(routes.login, login)
 globalRouter.get(routes.logout, logout)
 
 export default globalRouter
--- routes.js
+++ routes.js
@@ -25,12 +25,24 @@ const routes = {
     logout: LOGOUT,
     search: SEARCH,
     user: USER,
-    userDetail: USER_DETAIL,
+    userDetail: id => {
+        if (id) {
+            return `/users/${id}`
+        } else {
+            return USER_DETAIL
+        }
+    },
     editProfile: EDIT_PROFILE,
     changePassword: CHANGE_PASSWORD,
     videos: VIDEOS,
     upload: UPLOAD,
-    videoDetail: VIDEO_DETAIL,
+    videoDetail: id => {
+        if (id) {
+            return `/videos/${id}`
+        } else {
+            return VIDEO_DETAIL
+        }
+    },
     editVideo: EDIT_VIDEO,
     editVideo: EDIT_PROFILE,
     deleteVideo: DELETE_VIDEO,
