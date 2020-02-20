f4cbfa7b6ff3658c2ce446bad325dd25ed53a11d
taewo <moondingie5@gmail.com>
2020-02-20 17:07:35 +0900

    #2.4 Handling Routes

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ function handleListening() {
   console.log(`Listening on http://localhost:${PORT}`)
 }
 
+function handleHome(req, res) {
+  res.send('hi')
+}
+
+function handleProfile(req, res) {
+  res.send('handleProfile')
+}
+
+app.get('/', handleHome)
+app.get('/profile', handleProfile)
+
 app.listen(PORT, handleListening)
