722491cd40f1d05b87b4560a552a168dfbe9cea0
Taewoong Moon <moondingie5@gmail.com>
2020-02-29 01:06:30 +0900

    Auto stash before merge of "master" and "origin/master"

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,14 +1,9 @@
-export const home = (req, res) => 
-  res.render("home", { pageTitle: "Home" });
+export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
 export const search = (req, res) => {
-  const { query: { term: searchingBy }} = req
-  res.render("search", { pageTitle: "Search", searchingBy });
-}
-export const upload = (req, res) =>
-  res.render("upload", { pageTitle: "Upload" });
-export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Detail" });
-export const editVideo = (req, res) =>
-  res.render("editVideo", { pageTitle: "Edit Video" });
-export const deleteVideo = (req, res) =>
-  res.render("deleteVideo", { pageTitle: "Delete Video" });
+	const { query: { term: searchingBy } } = req;
+	res.render('search', { pageTitle: 'Search', searchingBy });
+};
+export const upload = (req, res) => res.render('upload', { pageTitle: 'Upload' });
+export const videoDetail = (req, res) => res.render('videoDetail', { pageTitle: 'Video Detail' });
+export const editVideo = (req, res) => res.render('editVideo', { pageTitle: 'Edit Video' });
+export const deleteVideo = (req, res) => res.render('deleteVideo', { pageTitle: 'Delete Video' });
