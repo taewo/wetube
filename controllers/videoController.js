852fc16d0206414a16ce6b08adf1ae09018c5c35
Taewoong Moon <moondingie5@gmail.com>
2020-02-29 01:47:07 +0900

    #2.21 Home controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,6 @@
-export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
+import { videos } from "../db";
+
+export const home = (req, res) => res.render('home', { pageTitle: 'Home', videos });
 export const search = (req, res) => {
 	const { query: { term: searchingBy } } = req;
 	res.render('search', { pageTitle: 'Search', searchingBy });
