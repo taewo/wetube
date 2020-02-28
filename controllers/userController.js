eebd73fe45f98bfe59f5aaf17435c4067221b104
Taewoong Moon <moondingie5@gmail.com>
2020-02-29 02:19:13 +0900

    #2.23 join controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,23 @@
-export const join = (req, res) => 
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // To Do: Register User
+    // To Do: Log user in
+    res.redirect(routes.home);
+  }
+};
+
 export const login = (req, res) =>
   res.render("login", { pageTitle: "Log In" });
 export const logout = (req, res) =>
