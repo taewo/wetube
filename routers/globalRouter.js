56c4c0c4b024dc0ace593228eee08f49f42a646b
Taewoong Moon <moondingie5@gmail.com>
2020-03-02 01:29:50 +0900

    #2.25 More controllers

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -17,7 +17,6 @@ globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.join, join);
 
 globalRouter.get(routes.login, getLogin);
 globalRouter.post(routes.login, postLogin);
