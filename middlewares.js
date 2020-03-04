cffec80aa3d262f117425cfd6b7db9a555d24e35
taewo <moondingie5@gmail.com>
2020-03-04 22:38:05 +0900

    #3.9 Editing a Video

--- middlewares.js
+++ middlewares.js
@@ -1,8 +1,6 @@
 import multer from "multer";
 import routes from "./routes"
-
 const multerVideo = multer({ dest: "uploads/videos/" });
-
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = 'WETUBE'
   res.locals.routes = routes
