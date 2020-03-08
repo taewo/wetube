8d1e7d618447298fcb3e46d0fefec386b49f01d8
Taewoong Moon <moondingie5@gmail.com>
2020-03-08 16:01:15 +0900

    #3.12 Searching Videos

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,9 +3,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({
-      _id: -1
-    });
+    const videos = await Video.find({}).sort({ _id: -1 });
     res.render("home", {
       pageTitle: "Home",
       videos
@@ -18,14 +16,19 @@ export const home = async (req, res) => {
     });
   }
 };
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
-  res.render("search", {
-    pageTitle: "Search",
-    searchingBy
-  });
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 export const getUpload = (req, res) =>
   res.render("upload", {
