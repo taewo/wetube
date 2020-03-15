c5b0c588111ad89a812531d3c4d416e91dd384e0
Taewoong Moon <moondingie5@gmail.com>
2020-03-15 11:10:11 +0900

    #5.1 Footer and Login Join

--- middlewares.js
+++ middlewares.js
@@ -5,7 +5,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = 'WETUBE'
   res.locals.routes = routes
   res.locals.user = {
-    isAuthenticated: true,
+    isAuthenticated: false,
     id: 1
   };
   next()
